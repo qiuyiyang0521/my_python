89d9fa0284df32387b4f6c849778001606691e3f
邱一阳 <104243947+qiuyiyang0521@users.noreply.github.com>
2022-07-02 16:34:27 +0800

    Add files via upload

--- 课后作业/海龟写名字.py
+++ 课后作业/海龟写名字.py
@@ -1,18 +1,9 @@
 import turtle as t
 
 t.bgcolor('black')
+t.pencolor("yellow")
 t.speed(0)
 your_name = t.textinput("名字","请输入你的名字")
-sides = int(t.numinput("边数","你想要几条边的螺旋线？",4,3,8))
-colors = ["red","yellow","green","blue","purple","pink","orange","white"]
-for x in range(100):
-    t.pencolor(colors[x % sides])
-    t.up()
-    t.forward(x * 4)
-    t.down()
-    t.write(your_name, font =("宋体", 12, "bold"))
-    t.left(360 / sides + 1)
+t.write(your_name, font = ("楷体",100,"bold"))
 
-
-print("绘制结束")
 
