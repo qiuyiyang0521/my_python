e6beb7d7db3652169d4cc365df772032685b3abc
邱一阳 <qiuyiyang0521@gmail.com>
2022-07-04 08:59:38 +0800

    更新 4-1.py

--- 课后作业/4-1.py
+++ 课后作业/4-1.py
@@ -1,6 +1,8 @@
-import turtle as t t.speed(0) num = int(t.numinput("圆", "你想要画几个圆", 4, 1,
-360)) t.pencolor(t.textinput("画笔颜色","你想要画出什么颜色？")) for x in range(1, num
-+ 1):
+import turtle as t
+t.speed(0)
+num = int(t.numinput("圆", "你想要画几个圆", 4, 1, 360))
+t.pencolor(t.textinput("画笔颜色","你想要画出什么颜色？"))
+for x in range(1, num + 1):
     t.circle(100)
     t.left(360 / num)
 t.done()
